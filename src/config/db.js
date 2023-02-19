60189de21dd2d3ac5617fbead9f98d227345cc05
Avijit Pateriya <deviljit.ap@gmail.com>
2023-02-19 13:13:24 +0530

    chnaged

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb+srv://avimessi10:DaN123@bia3@cluster0.g5lfyhm.mongodb.net/",
+  url: "mongodb+srv://avimessi10:DaN123@bia3@cluster0.g5lfyhm.mongodb.net",
   database: "images_db",
   imgBucket: "photos",
 };
