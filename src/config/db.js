bc07e8813c5e0b3c4d02940b09d96e3f9f4b9109
Avijit Pateriya <deviljit.ap@gmail.com>
2023-02-19 10:38:03 +0530

    final change

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb://localhost:27017",
+  url: "mongodb+srv://avimessi10:DaN123@bia3@cluster0.g5lfyhm.mongodb.net/",
   database: "images_db",
   imgBucket: "photos",
 };
